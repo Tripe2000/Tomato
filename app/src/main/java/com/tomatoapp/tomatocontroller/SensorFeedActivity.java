2613866b3e5092e1bf4a789371cf3cc1de3b76eb
Tripe2000 <tripe2000@gmail.com>
2019-01-01 20:11:09 -0500

    add SensorFeedActivity

---
+++ app/src/main/java/com/tomatoapp/tomatocontroller/SensorFeedActivity.java
@@ -0,0 +1,13 @@
+package com.tomatoapp.tomatocontroller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SensorFeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor_feed);
+    }
+}
