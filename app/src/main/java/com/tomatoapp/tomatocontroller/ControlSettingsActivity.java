ec4c8fff475381402f2d4a69a2da627476ba8f47
Tripe2000 <tripe2000@gmail.com>
2019-01-01 23:10:10 -0500

    added data persistence and confirmation button to sensor feed layout

--- app/src/main/java/com/tomatoapp/tomatocontroller/ControlSettingsActivity.java
+++ app/src/main/java/com/tomatoapp/tomatocontroller/ControlSettingsActivity.java
@@ -1,7 +1,13 @@
 package com.tomatoapp.tomatocontroller;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+import static com.tomatoapp.tomatocontroller.MainActivity.PACKAGE_NAME;
 
 public class ControlSettingsActivity extends AppCompatActivity {
 
@@ -9,5 +15,71 @@ public class ControlSettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control_settings);
+
+        //setup data persistence
+        final SharedPreferences prefs = this.getSharedPreferences(PACKAGE_NAME, Context.MODE_PRIVATE);
+
+        //setup listeners
+        Switch switch_A = (Switch) findViewById(R.id.switch2);
+        Switch switch_B = (Switch) findViewById(R.id.switch3);
+        Switch switch_C = (Switch) findViewById(R.id.switch4);
+        Switch switch_D = (Switch) findViewById(R.id.switch5);
+        Switch switch_E = (Switch) findViewById(R.id.switch6);
+        Switch switch_F = (Switch) findViewById(R.id.switch7);
+
+        switch_A.setChecked(prefs.getBoolean(PACKAGE_NAME + ".switch.a", true));
+        switch_B.setChecked(prefs.getBoolean(PACKAGE_NAME + ".switch.b", true));
+        switch_C.setChecked(prefs.getBoolean(PACKAGE_NAME + ".switch.c", true));
+        switch_D.setChecked(prefs.getBoolean(PACKAGE_NAME + ".switch.d", true));
+        switch_E.setChecked(prefs.getBoolean(PACKAGE_NAME + ".switch.e", true));
+        switch_F.setChecked(prefs.getBoolean(PACKAGE_NAME + ".switch.f", true));
+
+        switch_A.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putBoolean(PACKAGE_NAME + ".switch.a", isChecked);
+                editor.apply();
+            }
+        });
+
+        switch_B.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putBoolean(PACKAGE_NAME + ".switch.b", isChecked);
+                editor.apply();
+            }
+        });
+
+        switch_C.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putBoolean(PACKAGE_NAME + ".switch.c", isChecked);
+                editor.apply();
+            }
+        });
+
+        switch_D.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putBoolean(PACKAGE_NAME + ".switch.d", isChecked);
+                editor.apply();
+            }
+        });
+
+        switch_E.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putBoolean(PACKAGE_NAME + ".switch.e", isChecked);
+                editor.apply();
+            }
+        });
+
+        switch_F.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putBoolean(PACKAGE_NAME + ".switch.f", isChecked);
+                editor.apply();
+            }
+        });
     }
 }
--- app/src/main/java/com/tomatoapp/tomatocontroller/SensorFeedActivity.java
+++ app/src/main/java/com/tomatoapp/tomatocontroller/SensorFeedActivity.java
@@ -1,28 +1,58 @@
 package com.tomatoapp.tomatocontroller;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import static com.tomatoapp.tomatocontroller.MainActivity.PACKAGE_NAME;
+
 public class SensorFeedActivity extends AppCompatActivity {
+    SharedPreferences prefs;
+    SharedPreferences.Editor editor;
+    SeekBar seekBar_A;
+    SeekBar seekBar_B;
+    SeekBar seekBar_C;
+    TextView textView_A;
+    TextView textView_B;
+    TextView textView_C;
+
+    private void init() {
+        //setup data persistence
+        prefs = this.getSharedPreferences(PACKAGE_NAME, Context.MODE_PRIVATE);;
+        editor = prefs.edit();;
+
+        //widgets
+        seekBar_A = (SeekBar)findViewById(R.id.seekBar);
+        seekBar_B = (SeekBar)findViewById(R.id.seekBar2);
+        seekBar_C = (SeekBar)findViewById(R.id.seekBar3);
+        textView_A = (TextView)findViewById(R.id.textView4);
+        textView_B = (TextView)findViewById(R.id.textView7);
+        textView_C = (TextView)findViewById(R.id.textView12);
+    }
+
+    //widgets
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor_feed);
 
-        //add seekbar listeners
-        SeekBar seekBar_A = (SeekBar)findViewById(R.id.seekBar);
-        SeekBar seekBar_B = (SeekBar)findViewById(R.id.seekBar2);
-        SeekBar seekBar_C = (SeekBar)findViewById(R.id.seekBar3);
-        final TextView textView_A = (TextView)findViewById(R.id.textView4);
-        final TextView textView_B = (TextView)findViewById(R.id.textView7);
-        final TextView textView_C = (TextView)findViewById(R.id.textView12);
-        textView_A.setText(String.valueOf(seekBar_A.getProgress()));
-        textView_B.setText(String.valueOf(seekBar_B.getProgress()));
-        textView_C.setText(String.valueOf(seekBar_C.getProgress()));
+        //init variables
+        init();
 
+        //set value of widgets
+        textView_A.setText(String.valueOf(prefs.getInt(PACKAGE_NAME + ".textview.a", 5)));
+        textView_B.setText(String.valueOf(prefs.getInt(PACKAGE_NAME + ".textview.b", 5)));
+        textView_C.setText(String.valueOf(prefs.getInt(PACKAGE_NAME + ".textview.c", 5)));
+        seekBar_A.setProgress(Integer.parseInt(textView_A.getText().toString()));
+        seekBar_B.setProgress(Integer.parseInt(textView_B.getText().toString()));
+        seekBar_C.setProgress(Integer.parseInt(textView_C.getText().toString()));
+
+        //add seekbar listeners
         seekBar_A.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -58,5 +88,12 @@ public class SensorFeedActivity extends AppCompatActivity {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {}
         });
+    }
+
+    public void apply(View view) {
+        editor.putInt(PACKAGE_NAME + ".textview.a", Integer.parseInt(textView_A.getText().toString()));
+        editor.putInt(PACKAGE_NAME + ".textview.b", Integer.parseInt(textView_B.getText().toString()));
+        editor.putInt(PACKAGE_NAME + ".textview.c", Integer.parseInt(textView_C.getText().toString()));
+        editor.apply();
     }
 }
