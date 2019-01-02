5c116449390e302c650034c98251bd2bd08c4371
Tripe2000 <tripe2000@gmail.com>
2019-01-01 21:38:26 -0500

    more interface code

--- app/src/main/java/com/tomatoapp/tomatocontroller/SensorFeedActivity.java
+++ app/src/main/java/com/tomatoapp/tomatocontroller/SensorFeedActivity.java
@@ -3,8 +3,9 @@ package com.tomatoapp.tomatocontroller;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
-public class SensorFeedActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+public class SensorFeedActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,20 +16,47 @@ public class SensorFeedActivity extends AppCompatActivity implements SeekBar.OnS
         SeekBar seekBar_A = (SeekBar)findViewById(R.id.seekBar);
         SeekBar seekBar_B = (SeekBar)findViewById(R.id.seekBar2);
         SeekBar seekBar_C = (SeekBar)findViewById(R.id.seekBar3);
-        seekBar_A.setOnSeekBarChangeListener(this);
-        seekBar_B.setOnSeekBarChangeListener(this);
-        seekBar_C.setOnSeekBarChangeListener(this);
-    }
+        final TextView textView_A = (TextView)findViewById(R.id.textView4);
+        final TextView textView_B = (TextView)findViewById(R.id.textView7);
+        final TextView textView_C = (TextView)findViewById(R.id.textView12);
+        textView_A.setText(String.valueOf(seekBar_A.getProgress()));
+        textView_B.setText(String.valueOf(seekBar_B.getProgress()));
+        textView_C.setText(String.valueOf(seekBar_C.getProgress()));
 
-    @Override
-    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-    }
+        seekBar_A.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView_A.setText(String.valueOf(progress));
+            }
 
-    @Override
-    public void onStartTrackingTouch(SeekBar seekBar) {
-    }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
 
-    @Override
-    public void onStopTrackingTouch(SeekBar l) {
+        seekBar_B.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView_B.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+        seekBar_C.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView_C.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
     }
 }
